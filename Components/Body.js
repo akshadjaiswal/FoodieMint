bcbd402c4e716f05a109616a83c87bb94c8790ee
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-01 16:54:28 +0530

    top rated restaurant filtering function

--- Components/Body.js
+++ Components/Body.js
@@ -1,15 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search"></div>
-        <div className="res-container">
-          {resList.map((restaurant) => (
-            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-          ))}
-        </div>
-      </div> 
-    );
-  };
-  export default Body;
+  const [listOfRestaurants, setFilteredList] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setFilteredList(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
