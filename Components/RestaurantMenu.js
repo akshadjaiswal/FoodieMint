fe0f61d53f72ae0e9fac716bb918d5ee3cac2590
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-14 14:30:35 +0530

    Update price

--- Components/RestaurantMenu.js
+++ Components/RestaurantMenu.js
@@ -36,7 +36,7 @@ const RestaurantMenu = () => {
         {itemCards.map((item) => (
           <li key={item.card.info.id}>
             {item.card.info.name}- {"Rs."}
-            {item.card.info.price / 100}
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
           </li>
         ))}
       </ul>
