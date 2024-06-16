3c10e7996f11ad2c74118d1efa1e8dfe90d8a415
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-06-16 23:33:37 +0530

    omment

--- Components/About.js
+++ Components/About.js
@@ -1,9 +1,9 @@
 const About = () => {
-    return (
-        <div className="About-div">
-            <h1>About</h1>
-            <h2>This is about page of FoodieMint web app</h2>
-        </div>
-    )
-}
+  return (
+    <div className="About-div">
+      <h1>About</h1>
+      {/* <h2>This is about page of FoodieMint web app</h2> */}
+    </div>
+  );
+};
 export default About;
