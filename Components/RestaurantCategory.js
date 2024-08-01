8d8913339b929eaf703f5cbfe8567d4da4930c32
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-08-01 14:26:36 +0530

    Updating Restaurant Menu Page

--- Components/RestaurantCategory.js
+++ Components/RestaurantCategory.js
@@ -1,10 +1,16 @@
+import ItemList from "./ItemList";
 const RestaurantCategory = ({ data }) => {
-  console.log(data);
+  // console.log(data);
   return (
     <div>
-      <div>
-        <span>{data.title}</span>
-        <span>⬇️</span>
+      <div className="w-6/12 bg-slate-100 shadow-lg rounded-sm p-4 mx-auto my-4  font-serif">
+        <div className="flex justify-between">
+          <span className="font-bold text-lg">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span> 🔽 </span>
+        </div>
+        <ItemList items={data.itemCards} />
       </div>
     </div>
   );
